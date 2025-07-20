552b829987e93b2c0ff9357aaef94e88acbf9847
Roberto de Oliveira <roberto.oliveira@tqmwork.com>
2025-07-20 19:03:04 -0300

    feat: post a new recado

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,12 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException, Post, Body } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
-    // Find all recados
-    findAll() {
-        return 'This route will return all recados';
+
+  private readonly recados = [`Recado 1`, `Recado 2`, `Recado 3`];
+  @Get()
+  findAll() {
+    return this.recados;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') grdId: number) {
+    const recado= this.recados[--grdId];
+    if (!recado) {
+      throw new NotFoundException(`Message not found for id: ${grdId}`);
     }
-    // Find one recado by ID
-    findOne() {
-        return 'This route will return a specific recado by ID';
-    }
+    return recado;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
+}
+
+
+
