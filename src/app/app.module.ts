c7cb3d22c75ce9dcb245882049b9b485f1adc4a4
Roberto de Oliveira <roberto.oliveira@tqmwork.com>
2025-08-27 22:05:05 -0300

    feat: database connectivity in nestjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RecadosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'recados123',
+      password: 'recados123',
+      database: 'recadosdb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      logging: true,
+      autoLoadEntities: true,
+      synchronize: true, //nunca usar em produção
+    }),
+    RecadosModule,
+
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
