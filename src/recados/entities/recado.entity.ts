e86de9495da8dd7082b1330a397ec58eb5d411b5
Roberto de Oliveira <roberto.oliveira@tqmwork.com>
2025-09-17 20:57:31 -0300

    feat: config table recados

--- src/recados/entities/recado.entity.ts
+++ src/recados/entities/recado.entity.ts
@@ -1,8 +1,30 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity() 
 export class Recado {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({type: `varchar`, length: 255})
   texto: string;
+
+  @Column({type: `varchar`, length: 50})
   de: string;
+
+  @Column({type: `varchar`, length: 50})
   para: string;
+
+  @Column({type: `boolean`, default: false})
   lido: boolean;
+
+  @Column({type: `timestamp`, default: () => `CURRENT_TIMESTAMP`})
   data: Date;
+
+  @CreateDateColumn()
+  criadoEm?: Date;
+
+  @UpdateDateColumn()
+  atualizadoEm?: Date;
+
+  
 }
