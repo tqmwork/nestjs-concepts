cdc3c0b0d8eb2e2e72217b3f9ab22a20df9bf371
Roberto de Oliveira <roberto.oliveira@tqmwork.com>
2025-10-23 20:23:35 -0300

    feat: create pessoa dto

--- src/pessoa/dto/create-pessoa.dto.ts
+++ src/pessoa/dto/create-pessoa.dto.ts
@@ -1,1 +1,22 @@
-export class CreatePessoaDto {}
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreatePessoaDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(100)
+    readonly nome: string; 
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    @MaxLength(100)
+    readonly email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    @MaxLength(6)
+    readonly senha: string;
+}
