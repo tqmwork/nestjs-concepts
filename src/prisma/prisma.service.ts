d33e0ccb985d3c022c30c625708c5a51270bc33d
Roberto de Oliveira <roberto.oliveira@tqmwork.com>
2025-11-27 21:32:08 -0300

    feat: prisma service adjustment

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  
+
   constructor() {
     super({
       log: ['warn', 'error'],
